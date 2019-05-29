800e094cc141a558ec0d55895669c4b73eaa48e6
Nokia3310Team <icake.cc@gmail.com>
2019-05-29 12:35:47 +0200

    new version
    js script fix

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-document.getElementById('1').addEventListener('click', function() {
-  location.href = 'google.com'
-}, false);
+function mySecret() {
+  document.getElementById("secret").innerHTML = "You found a secret. Congratulation.";
+}
